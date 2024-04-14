b8f35a2cb9ad5b2546f0ce716a63f0e226c1dbc4
tync0 <isa.quluyev.wip@gmail.com>
2024-04-15 02:42:52 +0400

    Type validation within route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Python script that starts a Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return f'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return f'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
