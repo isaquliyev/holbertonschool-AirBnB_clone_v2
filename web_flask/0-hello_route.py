47b464877b9d6e24983097c9a29c90c5a354034b
tync0 <isa.quluyev.wip@gmail.com>
2024-04-15 02:22:57 +0400

    Module cleaned

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Python script that starts a Flask web application"""
+
+
 from flask import Flask
 
 
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return f'Hello HBNB!'
