a87593ea0458554b2243760ebd9015985ff0a748
tync0 <isa.quluyev.wip@gmail.com>
2024-04-15 16:52:08 +0400

    Fetching data for each request

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,11 +8,11 @@ from models.state import State
 
 
 app = Flask(__name__)
-depo = storage.all(State).values()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    depo = storage.all(State).values()
     return render_template("7-states_list.html", depo=depo)
 
 
