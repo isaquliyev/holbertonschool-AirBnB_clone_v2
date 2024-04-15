7938a49ee601b9599c75cd4b236edbc078dc4946
tync0 <isa.quluyev.wip@gmail.com>
2024-04-15 16:46:05 +0400

    Sorting in Jinja

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,7 @@ from models.state import State
 
 
 app = Flask(__name__)
-depo = sorted(storage.all(State).values(), key=lambda x: x.name)
+depo = storage.all(State).values()
 
 
 @app.route('/states_list', strict_slashes=False)
