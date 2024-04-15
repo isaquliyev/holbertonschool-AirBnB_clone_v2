8232940147f82f0e450f6e5b0afb425fa3568f59
tync0 <isa.quluyev.wip@gmail.com>
2024-04-15 15:51:42 +0400

    Pycodestyle validation

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,5 +46,6 @@ def number_odd_or_even(n):
     s = "even" if n % 2 == 0 else "odd"
     return render_template('6-number_odd_or_even.html', n=n, state=s)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
