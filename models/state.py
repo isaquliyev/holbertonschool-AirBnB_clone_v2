3a8d4cb4993cf138fae90442b00635addda2c697
tync0 <isa.quluyev.wip@gmail.com>
2024-04-15 16:04:55 +0400

    City imported correctly

--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
-from models import City
+from models.city import City
 
 
 class State(BaseModel, Base):
